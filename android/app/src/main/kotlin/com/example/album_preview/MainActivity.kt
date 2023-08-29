e6f627633afee36d4aa7410ab83bb9f55cf712a3
JH <wogks27@naver.com>
2023-08-29 10:25:22 +0900

    Feat: music player screen

---
+++ android/app/src/main/kotlin/com/example/album_preview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.album_preview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/music_player_screen.dart
@@ -0,0 +1,93 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class MusicPlayerScreen extends StatefulWidget {
+  const MusicPlayerScreen({super.key});
+
+  @override
+  State<MusicPlayerScreen> createState() => _MusicPlayerScreenState();
+}
+
+class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
+  final PageController _pageController = PageController(viewportFraction: 0.8);
+
+  int _currentPage = 0;
+
+  void _onPageChanged(int newPage) {
+    setState(() {
+      _currentPage = newPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 500),
+            child: Container(
+              key: ValueKey(_currentPage),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/covers/${_currentPage + 1}.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 15,
+                  sigmaY: 15,
+                ),
+                child: Container(
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+            ),
+          ),
+          PageView.builder(
+            onPageChanged: _onPageChanged,
+            controller: _pageController,
+            scrollDirection: Axis.horizontal,
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 350,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.4),
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                          offset: const Offset(0, 8),
+                        )
+                      ],
+                      image: DecorationImage(
+                        image: AssetImage('assets/covers/${index + 1}.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 35),
+                  const Text(
+                    'Interstellar',
+                    style: TextStyle(fontSize: 26, color: Colors.white),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text('Hans Zimmer',
+                      style: TextStyle(fontSize: 18, color: Colors.white)),
+                ],
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
